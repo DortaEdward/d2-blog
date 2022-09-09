413000db58de3dbc6046cbbb06c82f6b6bc9ab45
Edward Dorta <dortaedward@gmail.com>
2022-09-09 16:44:44 -0400

    displaying content

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { trpc } from "@/utils/trpc";
+import Image from "next/image";
+import { marked } from "marked";
 
 const PostPageContent: React.FC<{ id: string }> = ({ id }) => {
+  const createMarkup = (content: string) => {
+    return { __html: content };
+  };
+
   const { data, isLoading, error } = trpc.useQuery(["get-post-by-id", { id }]);
-  return <div className="max-w-screen-lg w-full">{JSON.stringify(data)}</div>;
+  if (!data?.content || typeof data?.content !== "string")
+    return <h1>No Content</h1>;
+  const parsedContent = marked.parse(data?.content);
+  return (
+    <div className="max-w-screen-lg w-full flex flex-col items-center">
+      <Image src={data?.imageUrl} alt="Post Image" width={700} height={400} />
+      <div dangerouslySetInnerHTML={createMarkup(data.content)} />
+    </div>
+  );
 };
 
 const PostPage: React.FC = () => {
