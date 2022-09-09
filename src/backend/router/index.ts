c64ed0a60919af1d45faa6356d5e6f0426038a72
Edward Dorta <dortaedward@gmail.com>
2022-09-08 22:18:18 -0400

    added slug creation on post creation

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -3,7 +3,7 @@ import * as trpc from "@trpc/server";
 import { resolve } from "path";
 import { z } from "zod";
 const { marked } = require("marked");
-// const slugify = require("slugify");
+const slugify = require("slugify");
 const createDomPurify = require("dompurify");
 const { JSDOM } = require("jsdom");
 const dompurify = createDomPurify(new JSDOM().window);
@@ -20,14 +20,17 @@ export const appRouter = trpc
       title: z.string(),
       content: z.string(),
       imageUrl: z.string(),
+      slug: z.string(),
     }),
     async resolve({ input }) {
       input.content = dompurify.sanitize(marked(input.content));
+      input.slug = slugify(input.title, { lower: true, strict: true });
       return await prisma.post.create({
         data: {
           title: input.title,
           content: input.content,
           imageUrl: input.imageUrl,
+          slug: input.slug,
         },
       });
     },
